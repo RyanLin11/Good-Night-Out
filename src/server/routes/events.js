da23a30720742d7cb79e4ef99455d5daac66bb2b
Joseph Wang <44278515+EmeraldEntities@users.noreply.github.com>
2022-01-03 16:20:29 -0500

    fistr change for functionality

--- src/server/routes/events.js
+++ src/server/routes/events.js
@@ -1,40 +1,36 @@
 const express = require("express");
 const eventRoutes = express.Router();
 const db = require("../db/dao");
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require("mongodb");
 
-eventRoutes.route("/events/").get(async function(req, res) {
-    console.log("Hello there");
-    await db.connectToServer();
-    const events = await db.findMatchingEvents("");
-    res.json(events);
+eventRoutes.route("/events/").get(async function (req, res) {
+	console.log("Hello there");
+	const events = await db.events.findMatchingEvents("");
+	res.json(events);
 });
 
-eventRoutes.route("/events/:id").get(async function(req, res) {
-    await db.connectToServer();
-    const event = await db.getEvent(req.params.id);
-    res.json(event);
+eventRoutes.route("/events/:id").get(async function (req, res) {
+	const event = await db.events.getEvent(req.params.id);
+	res.json(event);
 });
 
-eventRoutes.route("/events/add").post(async function(req, res) {
-    await db.connectToServer();
-    db.addBasicEvent(req.body.name, req.body.date, true);
+eventRoutes.route("/events/add").post(async function (req, res) {
+	db.events.addBasicEvent(req.body.name, req.body.date, true);
 });
 
-eventRoutes.route("/events/:id").delete(async function(req, res) {
-    console.log(`proceed to delete ${req.params.id} on server`);
-    await db.connectToServer();
-    const result = await db.deleteEvent(ObjectId(req.params.id));
-    console.log(result);
-    if(result) {
-        return "Event was successfully deleted.";
-    } else {
-        return "Event does not exist.";
-    }
-})
+eventRoutes.route("/events/:id").delete(async function (req, res) {
+	console.log(`proceed to delete ${req.params.id} on server`);
+	const result = await db.events.deleteEvent(req.params.id);
+	console.log(result);
+	if (result) {
+		return "Event was successfully deleted.";
+	} else {
+		return "Event does not exist.";
+	}
+});
 
-eventRoutes.route("/event/:id/update").get(async function(req, res) {
-    await db.connectToServer();
-})
+eventRoutes.route("/event/:id/update").get(async function (req, res) {
+	// await db.connectToServer();
+});
 
 module.exports = eventRoutes;
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,25 +1,30 @@
-const express = require('express');
+const express = require("express");
 const userRoutes = express.Router();
-const db = require('../db/dao');
-const {ObjectId} = require('mongodb');
-const { addBasicUser, getUser } = require('../db/dao');
+const db = require("../db/dao");
 
-userRoutes.route("/users/login/").post(async function(req, res) {
-    console.log("Logging in..");
-    await db.connectToServer();
-    const formattedUsername = req.body.username.padEnd(12, '.').substring(0, 12);
-    let db_user = await db.getUser(formattedUsername);
-    if(db_user == null) {
-        await db.addBasicUser(req.body.username, req.body.password, formattedUsername, req.body.email);
-        db_user = await db.getUser(formattedUsername);
-    }
-    console.log(db_user);
-    res.json(db_user);
+userRoutes.route("/users/login/").post(async function (req, res) {
+	console.log("Logging in..");
+
+	const formattedUsername = req.body.username.padEnd(12, ".").substring(0, 12);
+	let db_user = await db.users.getUser(formattedUsername);
+	if (db_user == null) {
+		await db.users.addBasicUser(
+			req.body.username,
+			req.body.password,
+			formattedUsername,
+			req.body.email
+		);
+		db_user = await db.users.getUser(formattedUsername);
+	}
+
+	console.log(db_user);
+	res.json(db_user);
 });
 
-userRoutes.route("/users/:name/").get(async function(req, res) {
-    await db.connectToServer();
-    return await getUser(req.params.name);
+userRoutes.route("/users/:name/").get(async function (req, res) {
+	const user = await db.users.getUser(req.params.name);
+
+	res.json(user);
 });
 
 module.exports = userRoutes;
