72e590cba2d3524697806194c311eb2fefe5ad88
Joseph Wang <44278515+EmeraldEntities@users.noreply.github.com>
2022-01-03 22:59:28 -0500

    added and tested user adding through API

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -2,31 +2,63 @@ const express = require("express");
 const userRoutes = express.Router();
 const db = require("../db/dao");
 
-//! Not tested yet, no authentication currently
-userRoutes.route("/api/users/login/").post(async function (req, res) {
-	console.log("Logging in..");
+//! We don't handle authentication so this is commented out for now
+// userRoutes.route("/api/users/login/").post(async function (req, res) {
+// 	console.log("Logging in..");
 
-	//? What is the point of this?
-	const formattedUsername = req.body.username.padEnd(12, ".").substring(0, 12);
-	let db_user = await db.users.getUser(formattedUsername);
+//? What is the point of this?
+// const formattedUsername = req.body.username.padEnd(12, ".").substring(0, 12);
+
+// 	let db_user = await db.users.getUser(formattedUsername);
+// 	if (db_user == null) {
+// 		await db.users.addBasicUser(
+// 			req.body.username,
+// 			req.body.password,
+// 			formattedUsername,
+// 			req.body.email
+// 		);
+// 		db_user = await db.users.getUser(formattedUsername);
+// 	}
+
+// 	console.log(db_user);
+// 	res.json(db_user);
+// });
+
+userRoutes.route("/api/users/add").post(async function (req, res) {
+	let db_user = await db.users.getUser(req.body.username);
 	if (db_user == null) {
 		await db.users.addBasicUser(
+			req.body.firstname,
+			req.body.lastname,
 			req.body.username,
-			req.body.password,
-			formattedUsername,
 			req.body.email
 		);
-		db_user = await db.users.getUser(formattedUsername);
+
+		db_user = await db.users.getUser(req.body.username);
 	}
 
-	console.log(db_user);
-	res.json(db_user);
+	// if it failed, return an empty object
+	res.json(db_user ? db_user : {});
 });
 
 userRoutes.route("/api/users/:name/").get(async function (req, res) {
 	const user = await db.users.getUser(req.params.name);
 
 	res.json(user);
+});
+
+// Gets the events that this user is participating in
+userRoutes.route("/api/users/:name/participating").get(async function (req, res) {
+	const items = await db.users.getParticipatingIn(req.params.name);
+
+	res.json(items);
+});
+
+// Gets the events that this user has created
+userRoutes.route("/api/users/:name/events").get(async function (req, res) {
+	const items = await db.users.getCreatedEvents(req.params.name);
+
+	res.json(items);
 });
 
 module.exports = userRoutes;
